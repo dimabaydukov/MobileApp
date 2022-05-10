b9adcb3690bdf4f2a26abfc42128904e617169e2
Aily-Hhh <aily.hhh13@gmail.com>
2022-05-11 00:37:33 +0300

    update

--- app/src/main/java/com/example/dino/GrowADino.java
+++ app/src/main/java/com/example/dino/GrowADino.java
@@ -3,16 +3,22 @@ package com.example.dino;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+<<<<<<< Updated upstream
 import android.widget.SeekBar;
 import android.widget.TextView;
+=======
+>>>>>>> Stashed changes
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class GrowADino extends AppCompatActivity {
 
     Button timeButton;
+<<<<<<< Updated upstream
     TextView textViewForSeekBar;
     SeekBar seekBar;
+=======
+>>>>>>> Stashed changes
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +26,7 @@ public class GrowADino extends AppCompatActivity {
         setContentView(R.layout.grow_a_dino);
 
         timeButton = findViewById(R.id.timeButton);
+<<<<<<< Updated upstream
         textViewForSeekBar = findViewById(R.id.textViewForSeekBar);
         seekBar = findViewById(R.id.seekBar);
 
@@ -40,6 +47,8 @@ public class GrowADino extends AppCompatActivity {
 
             }
         });
+=======
+>>>>>>> Stashed changes
     }
 
     public void setTime(View v) {
